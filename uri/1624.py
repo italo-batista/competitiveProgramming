dd748f061f19e6dd5db6060d5bd3c95d2656ffe8
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2017-07-31 14:15:37 -0300

    Promoção (URI DP question)

---
+++ uri/1624.py
@@ -0,0 +1,30 @@
+MAX = 100
+n = int(raw_input())
+
+while n != 0:
+		
+	valores = [0] * (n+1)
+	pesos = [0] * (n+1) 
+
+	for i in xrange(n):	
+		p, P = map(int, raw_input().split())
+		valores[i] = p
+		pesos[i] = P
+		
+	m = int(raw_input())
+		
+	dp = []
+	for i in xrange(n+1):
+		array = []
+		for j in xrange(m+1):
+			array.append(0)
+		dp.append(array)
+					
+	for i in xrange(n+1):
+		for j in xrange(m+1):
+							
+			if i == 0 or j == 0:
+				dp[i][j] = 0
+
+			elif j < pesos[i-1]:
+				dp[i][j] = dp[i-1][j]
