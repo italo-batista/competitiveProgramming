296aabf6f4319ecfa0c4ec5fdf79f53249e6ffd0
Italo Batista <italohmb@gmail.com>
2017-11-08 00:01:49 -0300

    Update is_unique.py

--- cracking-code-interview/is_unique.py
+++ cracking-code-interview/is_unique.py
@@ -7,10 +7,10 @@ def is_unique(string):
   
   for i in xrange(len(string)):    
     value = ord(string[i].lowert()) - A_ASCII	
-    if (checker & (1 << value)) > 0:		              # Bitwise AND to check if value is already set on checker 
-			return False				                                 	
-    checker |= (1 << value)			                      # Turns 1 the position of current char (LSB: ‘a’, MSB:’z’) to say
-                                                      # that the char is already used.  
+    if (checker & (1 << value)) > 0:		        # Bitwise AND to check if value is already set on checker 
+	return False				                                 	
+    checker |= (1 << value)				# Turns 1 the position of current char (LSB: ‘a’, MSB:’z’) to say 
+							# that the char is already used.  
 
 								                                      
 string = input()
