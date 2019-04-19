b2477a744299d90b47af8047bccdf5ebc7bc644e
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-04-19 19:08:42 -0300

    Fix bug after update

--- cracking-code-interview/chapter_02/get_kth_to_last.py
+++ cracking-code-interview/chapter_02/get_kth_to_last.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append('./mcollections')
-from mcollections import LinkedList, Node
+sys.path.append('./linkedlist')
+from linkedlist import LinkedList, Node
 
 
 def get_ll_size(ll):
     size = 0
     cnode = ll.head
-    while cnode is not None:
+    while cnode.value is not None:
         size += 1
         cnode = cnode.next
     return size
@@ -24,12 +24,13 @@ def solver(ll, k):
     return cnode
 
 
+if __name__ == '__main__':  # tests
 
-node1 = Node(value=1)
-node2 = Node(value=2)
-node3 = Node(value=3)
-ll = LinkedList(head=node1)
-ll.append(node2)
-ll.append(node3)
+    node1 = Node(value=1)
+    node2 = Node(value=2)
+    node3 = Node(value=3)
+    ll = LinkedList(head=node1)
+    ll.append(node2)
+    ll.append(node3)
 
-assert solver(ll, 1) == node2
+    assert solver(ll, 1) == node2
