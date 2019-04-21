29cd90f804230306144dd3653666afa07dc21874
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2019-04-20 23:51:44 -0300

    Improve stack

--- cracking-code-interview/chapter_03/datastructures.py
+++ cracking-code-interview/chapter_03/datastructures.py
@@ -5,6 +5,13 @@ class StackNode:
         self.value = value
         self.min_value = min_value
 
+    def __str__(self):
+        return "StackNode{value: " + str(self.value) + " }\n"
+
+    def __repr__(self):
+        return self.__str__()
+
+
 class Stack:
     def __init__(self):
         self._array = []
@@ -13,7 +20,7 @@ class Stack:
         return self._array.pop().value
 
     def push(self, item):
-        new_min = item if self._is_less_than_min(item) else self.peek().min_value
+        new_min = item if self._is_less_than_min(item) else self._top_node().min_value
         new_item = StackNode(value=item, min_value=new_min)
         self._array.append(new_item)
 
@@ -21,18 +28,27 @@ class Stack:
         return len(self._array) == 0
 
     def peek(self):
+        return self._array[-1].value if len(self._array) > 0 else None
+
+    def _top_node(self):
         return self._array[-1] if len(self._array) > 0 else None
 
     def min(self):
-        return self.peek().min_value
+        return self._top_node().min_value
 
     def _is_less_than_min(self, item):
-        if self.peek() and item < self.peek().min_value:
+        if self.peek() and item < self._top_node().min_value:
             return True
         elif not self.peek():
             return True
         else:
             return False
+
+    def __str__(self):
+        return "[ " + " ".join(map(lambda node: str(node), self._array)) + " ]"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Queue:
